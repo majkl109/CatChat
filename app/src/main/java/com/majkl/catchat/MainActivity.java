58d574f79153686e430127b4007363205a9cab16
Majkl <majkl.andracic@gmail.com>
2020-07-13 10:09:57 +0200

    Drawers in progress

--- app/src/main/java/com/majkl/catchat/MainActivity.java
+++ app/src/main/java/com/majkl/catchat/MainActivity.java
@@ -1,6 +1,9 @@
 package com.majkl.catchat;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Fragment fragment = new InboxFragment();
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.add(R.id.content_frame,fragment);
+        ft.commit();
     }
 }
