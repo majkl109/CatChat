114fa7a8f73c4c069ac1ca07632d5ae5730d4f11
Majkl <majkl.andracic@gmail.com>
2020-07-18 11:01:23 +0200

    Drawers in progress

--- app/src/main/java/com/majkl/catchat/InboxFragment.java
+++ app/src/main/java/com/majkl/catchat/InboxFragment.java
@@ -13,14 +13,12 @@ public class InboxFragment extends Fragment {
 
 
     public InboxFragment() {
-        // Required empty public constructor
     }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_inbox, container, false);
     }
 
--- app/src/main/java/com/majkl/catchat/SentItemsFragment.java
+++ app/src/main/java/com/majkl/catchat/SentItemsFragment.java
@@ -10,21 +10,17 @@ import android.view.View;
 import android.view.ViewGroup;
 
 
-/**
- * A simple {@link Fragment} subclass.
- */
+
 public class SentItemsFragment extends Fragment {
 
 
     public SentItemsFragment() {
-        // Required empty public constructor
     }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_sent_items, container, false);
     }
 
